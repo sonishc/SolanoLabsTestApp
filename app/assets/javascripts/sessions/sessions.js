7ea20eff33e6cae01e4203587f6c0892d9776c99
sonish <onixsonic@gmail.com>
2018-11-14 22:53:31 +0200

    Changes for chart view

--- app/assets/javascripts/sessions/sessions.js
+++ app/assets/javascripts/sessions/sessions.js
@@ -29,6 +29,28 @@ function averageFails(obj) {
   return average
 }
 
+function get_fields(obj, created_at, field) {
+  var conut_of_errors_per_day = []
+  for (item in obj) {
+    if ( sessions[item]['summary_status'] == field){
+      index = created_at.indexOf(sessions[item]['created_at'].slice(0,10))
+      conut_of_errors_per_day.push(index)
+    }
+  }
+
+  var occurrences_obj = countRepeatedItem(conut_of_errors_per_day)
+  console.log(occurrences_obj);
+  return occurrences_obj;
+}
+
+function createData(obj, length) {
+  let arr_tmp = new Array(length)
+  for (x in obj) {
+    arr_tmp[x] = obj[x]
+  }
+  return arr_tmp;
+}
+
 function removeRepeatedItem(arr) {
   var arr = arr.filter( function( item, index, inputArray ) {
     return inputArray.indexOf(item) == index;
@@ -43,36 +65,4 @@ function countRepeatedItem(arr) {
   }
 
   return occurrences; 
-}
-
-function getDataForDatasets(data_objects, label_array, field) {
-  var duration_time_sort = []; 
-  for (i=0; i<label_array.length; i++ ) {
-    duration_time_sort.push([])
-  }
-
-  for (let i = 0; i < data_objects.length; i++) {
-    var index = label_array.indexOf(data_objects[i]['created_at'].slice(0,10))
-    if (data_objects[i]['summary_status'] == 'failed' || data_objects[i]['summary_status'] == 'stopped'){
-      duration_time_sort[index].push(data_objects[i][field] * -1);
-    }else {
-      duration_time_sort[index].push(data_objects[i][field]);
-    }
-  }
-
-  var counter = [];
-  for (let i = 0; i < duration_time_sort.length; i++) {
-    counter.push(duration_time_sort[i].length)
-  }
-
-  var duration_time_unsort = []
-  for (let i=0; i<Math.max(...counter); i++) {
-    tmp = []
-    for (let j=0; j<duration_time_sort.length; j++) {
-      tmp.push(duration_time_sort[j][i])
-    }
-    duration_time_unsort.push(tmp)
-  }
-
-  return duration_time_unsort;
 }
